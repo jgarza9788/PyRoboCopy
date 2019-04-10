3b0fd5d151da0dc440ea51a8fe87ae6c4e618218
jgarza9788 <jgarza9788@gmail.com>
2019-04-09 20:53:20 -0700

    init

---
+++ PyRoboCopy.py
@@ -0,0 +1,8 @@
+import os
+os.system(r'robocopy "D:\Share" "E:\Share" /S ')
+os.system(r'robocopy "D:\Music" "E:\Music" /S ')
+os.system(r'robocopy "D:\Installers" "E:\Installers" /S  ')
+os.system(r'robocopy "D:\UnityProjects" "E:\UnityProjects" /S') 
+os.system(r'robocopy "D:\Documents" "E:\Documents" /S  ')
+os.system(r'robocopy "D:\Scripts" "E:\Scripts" /S  ')
+
